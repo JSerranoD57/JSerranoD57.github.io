6edae5b71d6136402c32e2959b928ae6ef4a42a7
JSerranoD57 <jserranod@unbosque.edu.co>
2026-02-15 22:26:12 -0500

    Set site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import edgeoneAdapter from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://JSerranoD57.github.io'
   adapter: edgeoneAdapter(),
   output: 'static',
   integrations: [react()]
